2320dbcd9fca7776f0b59526ccfba43baf1a5fdb
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-01-13 17:41:23 +0900

    [add] エラーログの名称区別

--- minelab-iot-camera/upload_image.py
+++ minelab-iot-camera/upload_image.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
         )
     except BaseCustomError as e:
         # エラーハンドラを初期化
-        handler = ErrorHandler(log_file=f'{Util.get_root_dir()}/log/{os.path.splitext(os.path.basename(__file__))[0]}.log')
+        handler = ErrorHandler(log_file=f'{Util.get_root_dir()}/log/error-{os.path.splitext(os.path.basename(__file__))[0]}.log')
         handler.handle_error(e)
