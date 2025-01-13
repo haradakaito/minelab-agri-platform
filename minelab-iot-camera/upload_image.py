de64703e3caead608f17d469e2153f7984e0262e
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-01-13 14:37:57 +0900

    [add] device-config.yamlからプロジェクト名を取得

--- minelab-iot-camera/upload_image.py
+++ minelab-iot-camera/upload_image.py
@@ -1,9 +1,6 @@
 import os
-from lib import BaseCustomError, ErrorHandler, Util, Camera
+from lib import BaseCustomError, ErrorHandler, ConfigLoader, Util, Camera
 from api_service import APIService
-
-# 定数
-PROJECT_NAME = 'csi' # プロジェクト名
 
 if __name__ == "__main__":
     try:
@@ -14,6 +11,9 @@ if __name__ == "__main__":
         api_service = APIService()
         api_client  = api_service.get_apiclient()
 
+        # デバイス設定を取得
+        device_config = ConfigLoader(config_path=f'{Util.get_root_dir()}/config/device-config.yaml')
+
         # APIリクエストを送信
         response_text = api_client.send_request(
             request_path='images',
@@ -21,10 +21,10 @@ if __name__ == "__main__":
             payload={
                 'device_name' : Util.get_device_name(),
                 'image_data'  : Util.encode_base64(camera.encode_frame(frame=camera.capture(), ext='.jpg')),
-                'project_name': PROJECT_NAME,
+                'project_name': device_config.get('project_name'),
                 'timestamp'   : Util.get_timestamp()
             },
-            timeout=3
+            timeout=10
         )
     except BaseCustomError as e:
         # エラーハンドラを初期化
