369a771f587803ca89d8d26dc77938b9fb43a297
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-01-15 15:59:17 +0900

    [add] 暗号化用コードの実装

--- share/encrypt_iam-config.py
+++ share/encrypt_iam-config.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
 
         # 設定ファイルを更新
         for key in iam_config.get():
-            # 暗号化された状態で保存されているデータを復号化して再度暗号化
-            plain_text     = AESCodec(key=Util.get_mac_address()).decode(encrypted_data=iam_config.get(key)) # 復号化
-            encrypted_data = AESCodec(key=Util.get_mac_address()).encode(plaintext=plain_text)                  # 再度暗号化
+            # 設定ファイルから値を取得
+            encrypted_data = AESCodec(key=Util.get_mac_address()).encode(plaintext=iam_config.get(key))                  # 再度暗号化
             # 設定ファイルを更新
             iam_config.set(key, value=encrypted_data)
 
