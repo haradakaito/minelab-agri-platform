55add3b98012c538209bdc548fa6d599a2d28cd6
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-02-25 19:02:54 +0900

    [add] jsonからデコーダ―を指定

--- minelab-iot-gateway/decode_pcap2csv.py
+++ minelab-iot-gateway/decode_pcap2csv.py
@@ -1,9 +1,20 @@
+import json
 import importlib
 import config
-decoder = importlib.import_module(f'lib_gateway.{config.decoder}')
+from lib import Util, AESCodec
 
 if __name__ == "__main__":
     try:
+        # 設定ファイルの読み込み
+        with open(f"{Util.get_root_dir()}/config/config.json", "r", encoding="utf-8") as file:
+            _config = json.load(file)
+
+        # AES暗号化クラスを初期化
+        aes_codec = AESCodec(key=Util.get_mac_address())
+
+        # デコーダを読み込み
+        decoder = importlib.import_module(f"lib_gateway.{aes_codec.decrypt(encrypted_data=_config['CSIChanger']['DECODER'])}")
+
         # Pcapファイル名を入力
         pcap_filename = input('Pcapファイル名: ')
         # 拡張子がない場合は付与する
