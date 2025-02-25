7f33671a2f3420fee1fe826efadda613ac5e6c5f
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-02-25 17:11:25 +0900

    [add] リファクタリング

--- csichanger/csiexplorer.py
+++ csichanger/csiexplorer.py
@@ -1,76 +1,71 @@
 import time
 import importlib
 import config
-from plotters.AmpPhaPlotter import Plotter # Amplitude and Phase plotter
-decoder = importlib.import_module(f'decoders.{config.decoder}') # This is also an import
+from plotters.AmpPhaPlotter import Plotter
+decoder = importlib.import_module(f'decoders.{config.decoder}')
 
 def string_is_int(s):
-    '''
-    Check if a string is an integer
-    '''
+    """文字列が整数であるかをチェックする関数"""
     try:
         int(s)
         return True
     except ValueError:
         return False
 
-
 if __name__ == "__main__":
-    pcap_filename = input('Pcap file name: ')
+    pcap_filename = input('Pcapファイル名: ')
 
     if '.pcap' not in pcap_filename:
         pcap_filename += '.pcap'
     pcap_filepath = '/'.join([config.pcap_fileroot, pcap_filename])
 
     try:
+        # パケットデータを読み込む
         samples = decoder.read_pcap(pcap_filepath)
-    except FileNotFoundError:
-        print(f'File {pcap_filepath} not found.')
-        exit(-1)
-
-    if config.plot_samples:
-        plotter = Plotter(samples.bandwidth)
-
-    while True:
-        command = input('> ')
-
-        if 'help' in command:
-            print(config.help_str)
-
-        elif 'exit' in command:
-            break
-
-        elif ('-' in command) and \
-            string_is_int(command.split('-')[0]) and \
-            string_is_int(command.split('-')[1]):
-
-            start = int(command.split('-')[0])
-            end = int(command.split('-')[1])
-
-            for index in range(start, end+1):
+        # プロッターを初期化
+        if config.plot_samples:
+            plotter = Plotter(samples.bandwidth)
+        # パケットデータをプロットする
+        while True:
+            # コマンドを受け取る
+            command = input('> ')
+            # コマンドに応じて処理を行う
+            ## ヘルプメッセージを表示
+            if 'help' in command:
+                print(config.help_str)
+            ## プログラムを終了
+            elif 'exit' in command:
+                break
+            ## # ハイフンが含まれている場合、範囲指定として処理
+            elif ('-' in command) and string_is_int(command.split('-')[0]) and string_is_int(command.split('-')[1]):
+                # ハイフンで分割して範囲を取得
+                start = int(command.split('-')[0])
+                end   = int(command.split('-')[1])
+                # サンプルの範囲をプロット
+                for index in range(start, end+1):
+                    # CSIサンプルをプロンプトに表示
+                    if config.print_samples:
+                        samples.print(index)
+                    # CSIをプロット
+                    if config.plot_samples:
+                        csi = samples.get_csi(index, config.remove_null_subcarriers, config.remove_pilot_subcarriers)
+                        plotter.update(csi)
+                    # アニメーションを遅延
+                    time.sleep(config.plot_animation_delay_s)
+            ## コマンドが整数の場合、そのインデックスのCSIをプロット
+            elif string_is_int(command):
+                # インデックスを取得
+                index = int(command)
+                # CSIサンプルをプロンプトに表示
                 if config.print_samples:
                     samples.print(index)
+                # CSIをプロット
                 if config.plot_samples:
-                    csi = samples.get_csi(
-                        index,
-                        config.remove_null_subcarriers,
-                        config.remove_pilot_subcarriers
-                    )
-                    plotter.update(csi)
+                        csi = samples.get_csi(index, config.remove_null_subcarriers, config.remove_pilot_subcarriers)
+                        plotter.update(csi)
+            ## それ以外の場合、不明なコマンドとして処理
+            else:
+                print('Unknown command. Type help.')
 
-                time.sleep(config.plot_animation_delay_s)
-
-        elif string_is_int(command):
-            index = int(command)
-
-            if config.print_samples:
-                samples.print(index)
-            if config.plot_samples:
-                    csi = samples.get_csi(
-                        index,
-                        config.remove_null_subcarriers,
-                        config.remove_pilot_subcarriers
-                    )
-                    plotter.update(csi)
-        else:
-            print('Unknown command. Type help.')
+    except Exception as e:
+        print(e)
