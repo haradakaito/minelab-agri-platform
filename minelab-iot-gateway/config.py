42268760ca32585a78728f993e4c2bbc55cd2d88
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-02-25 20:47:10 +0900

    [add] csvに出力

--- minelab-iot-gateway/config.py
+++
@@ -1,5 +0,0 @@
-# Nullサブキャリアの削除設定
-remove_null_subcarriers = True
-
-# パイロットサブキャリアの削除設定
-remove_pilot_subcarriers = False
--- minelab-iot-gateway/decode_pcap2csv.py
+++ minelab-iot-gateway/decode_pcap2csv.py
@@ -1,6 +1,8 @@
 import json
 import importlib
 import numpy as np
+import pandas as pd
+
 from lib import Util, AESCodec, ErrorHandler
 
 if __name__ == "__main__":
@@ -12,18 +14,23 @@ if __name__ == "__main__":
         # AES暗号化クラスを初期化
         aes_codec = AESCodec(key=Util.get_mac_address())
 
-        # デコーダを読み込み
+        # デコーダの読み込み
         decoder = importlib.import_module(f"lib_gateway.{aes_codec.decrypt(encrypted_data=config['CSIChanger']['DECODER'])}")
 
         # pcapファイル名リストを取得
         root_path = Util.get_root_dir()
         for dirname in Util.get_dir_list(path=f"{root_path}/pcap"):
             for filename in Util.get_file_name_list(path=f"{root_path}/pcap/{dirname}", ext=".pcap"):
+                # Pcapファイルの読み込み
                 samples = decoder.read_pcap(pcap_filepath=f"{root_path}/pcap/{dirname}/{filename}")
-                for index in range(2):
-                    csi = samples.get_csi(index, aes_codec.decrypt(encrypted_data=config["CSIChanger"]["REMOVE_NULL_SUBCARRIERS"]), aes_codec.decrypt(encrypted_data=config["CSIChanger"]["REMOVE_PILOT_SUBCARRIERS"]))
-                    print("振幅：", np.abs(csi))
-                    print("位相：", np.angle(csi))
+                # データの抽出
+                csi_amp = [np.abs(samples.get_csi(index=index, rm_nulls=True, rm_pilots=False)) for index in range(2)]   # 振幅
+                csi_pha = [np.angle(samples.get_csi(index=index, rm_nulls=True, rm_pilots=False)) for index in range(2)] # 位相
+                # データをcsvで保存
+                df_amp = pd.DataFrame(csi_amp)
+                df_pha = pd.DataFrame(csi_pha)
+                df_amp.to_csv(f"{Util.get_root_dir()}/csv/{dirname}/amp/{Util.remove_extention(file_name=filename)}.csv")
+                df_pha.to_csv(f"{Util.get_root_dir()}/csv/{dirname}/pha/{Util.remove_extention(file_name=filename)}.csv")
 
     except Exception as e:
         # エラーハンドラを初期化
