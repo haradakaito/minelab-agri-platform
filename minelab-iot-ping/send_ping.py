fcf15f1171d311455a5e0d2dc1514680bc64d393
muumin <harada.kaito.20@shizuoka.ac.jp>
2025-02-25 22:01:20 +0900

    [add] ループ内でエラー発生時，ログ出力・処理継続

--- minelab-iot-ping/send_ping.py
+++ minelab-iot-ping/send_ping.py
@@ -15,14 +15,20 @@ if __name__ == '__main__':
         # Pingを一定時間，一定間隔で送信
         end_time = datetime.now() + timedelta(seconds=int(config["Duration"])) # 終了時間（開始時間 + Duration秒）
         while datetime.now() < end_time:
-            # Ping送信
-            ping.send(
-                destination = str(config["Destination"]), # 送信先ホスト
-                timeout     = int(config["Timeout"])      # タイムアウト時間
-            )
-            # インターバル時間だけ待機
-            time.sleep(int(config["Interval"]))
+            try:
+                # Ping送信
+                ping.send(
+                    destination = str(config["Destination"]), # 送信先ホスト
+                    timeout     = int(config["Timeout"])      # タイムアウト時間
+                )
+                # インターバル時間だけ待機
+                time.sleep(int(config["Interval"]))
 
+            except Exception as e:
+                # エラーハンドラを初期化
+                handler = ErrorHandler(log_file=f'{Util.get_root_dir()}/log/{Util.get_exec_file_name()}.log')
+                handler.log_error(e)
+                continue
     except Exception as e:
         # エラーハンドラを初期化
         handler = ErrorHandler(log_file=f'{Util.get_root_dir()}/log/{Util.get_exec_file_name()}.log')
